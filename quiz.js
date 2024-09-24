580f44a292a6d48e9afdbcb4ff73e8f14a3c197b
Chidera Mabel Umejuru <deraumejuru@gmail.com>
2024-09-24 13:12:54 +0100

    added to the repo

--- quiz.js
+++ quiz.js
@@ -7,5 +7,86 @@ const questions = [
             { text:"40", correct:true },
             { text:"30", correct:false }
         ]
+    },
+    {
+        question: "How old was Saul when he became king?",
+        answers:  [
+            { text:"60", correct:false },
+            { text:"50", correct:false },
+            { text:"40", correct:true },
+            { text:"30", correct:false }
+        ]
+    },
+    {
+        question: "How old was Saul when he became king?",
+        answers:  [
+            { text:"60", correct:false },
+            { text:"50", correct:false },
+            { text:"40", correct:true },
+            { text:"30", correct:false }
+        ]
+    },
+    {
+        question: "How old was Saul when he became king?",
+        answers:  [
+            { text:"60", correct:false },
+            { text:"50", correct:false },
+            { text:"40", correct:true },
+            { text:"30", correct:false }
+        ]
+    },
+    {
+        question: "How old was Saul when he became king?",
+        answers:  [
+            { text:"60", correct:false },
+            { text:"50", correct:false },
+            { text:"40", correct:true },
+            { text:"30", correct:false }
+        ]
+    },
+    {
+        question: "How old was Saul when he became king?",
+        answers:  [
+            { text:"60", correct:false },
+            { text:"50", correct:false },
+            { text:"40", correct:true },
+            { text:"30", correct:false }
+        ]
+    },
+    {
+        question: "How old was Saul when he became king?",
+        answers:  [
+            { text:"60", correct:false },
+            { text:"50", correct:false },
+            { text:"40", correct:true },
+            { text:"30", correct:false }
+        ]
+    },
+    {
+        question: "How old was Saul when he became king?",
+        answers:  [
+            { text:"60", correct:false },
+            { text:"50", correct:false },
+            { text:"40", correct:true },
+            { text:"30", correct:false }
+        ]
+    },
+    {
+        question: "How old was Saul when he became king?",
+        answers:  [
+            { text:"60", correct:false },
+            { text:"50", correct:false },
+            { text:"40", correct:true },
+            { text:"30", correct:false }
+        ]
+    },
+    {
+        question: "How old was Saul when he became king?",
+        answers:  [
+            { text:"60", correct:false },
+            { text:"50", correct:false },
+            { text:"40", correct:true },
+            { text:"30", correct:false }
+        ]
     }
 ]
