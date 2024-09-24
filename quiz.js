e41e9739bea025b287b43b5c05a0f4058bbc922d
Chidera Mabel Umejuru <deraumejuru@gmail.com>
2024-09-24 13:10:09 +0100

    added to the repo

--- quiz.js
+++ quiz.js
@@ -1,6 +1,8 @@
 const questions = [
     {
-        question: "How old was Saul when he became king"
-        answer:  []
+        question: "How old was Saul when he became king?",
+        answers:  [
+            {text:"60" correct:false}
+        ]
     }
 ]
