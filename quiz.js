31ddf3c04776c834f066d916c49be6d491f418a0
Chidera Mabel Umejuru <deraumejuru@gmail.com>
2024-09-24 13:14:22 +0100

    added to the repo

--- quiz.js
+++ quiz.js
@@ -9,7 +9,7 @@ const questions = [
         ]
     },
     {
-        question: "How old was Saul when he became king?",
+        question: "How old was Moses when he died?",
         answers:  [
             { text:"60", correct:false },
             { text:"50", correct:false },
@@ -18,12 +18,12 @@ const questions = [
         ]
     },
     {
-        question: "How old was Saul when he became king?",
+        question: "Who was the first female judge in the Bible?",
         answers:  [
-            { text:"60", correct:false },
-            { text:"50", correct:false },
-            { text:"40", correct:true },
-            { text:"30", correct:false }
+            { text:"Hannah", correct:false },
+            { text:"Ruth", correct:false },
+            { text:"Mary", correct:false },
+            { text:"Deborah", correct:true }
         ]
     },
     {
