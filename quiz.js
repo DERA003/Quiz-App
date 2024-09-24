949080443f3b532fdbaf5c535457a64f440e7e16
Chidera Mabel Umejuru <deraumejuru@gmail.com>
2024-09-24 13:25:56 +0100

    added to the repo

--- quiz.js
+++ quiz.js
@@ -54,11 +54,11 @@ const questions = [
         ]
     },
     {
-        question: "How old was Saul when he became king?",
+        question: "Who was the last son of Jacob?",
         answers:  [
-            { text:"60", correct:false },
-            { text:"50", correct:false },
-            { text:"40", correct:true },
+            { text:"Joseph", correct:false },
+            { text:"Gideon", correct:false },
+            { text:"isaiah", correct:true },
             { text:"30", correct:false }
         ]
     },
