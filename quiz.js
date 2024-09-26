f78cc7489c48f65bddc7ac718fcaf1b60af86b55
Chidera Mabel Umejuru <deraumejuru@gmail.com>
2024-09-26 13:16:40 +0100

    added to the repo

--- quiz.js
+++ quiz.js
@@ -138,7 +138,7 @@ function selectAnswer(e) {
     const isCorrect = selectedBtn.dataset.correct === "true"
     if(isCorrect){
        selectedBtn.classList.add("correct")
-    }else(isCorrect){
-        selectedBtn.classList.add("correct")
+    }else{
+        selectedBtn.classList.add("incorrect")
      }
 }
